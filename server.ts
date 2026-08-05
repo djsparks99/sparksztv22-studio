33f6e0ea37d334a3352f029a18d71cc2510fb26e
djsparks99 <104692685+djsparks99@users.noreply.github.com>
2026-08-05 21:37:12 +0100

    fix

--- server.ts
+++ server.ts
@@ -621,7 +621,7 @@ async function startServer() {
       let html = fs.readFileSync(indexPath, "utf8");
 
       let title = "SPARKZ.TV // Your Stream, Your Mix, Your Rules";
-      let image = `${req.protocol}://${req.get("host")}/og-image.jpg`;
+      let image = `${req.protocol}://${req.get("host")}/og-image.png`;
       const url = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
 
       if (req.path.startsWith("/channel/")) {
